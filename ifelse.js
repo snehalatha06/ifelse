e9c5dbd1d6ce5ea041b450d465621ff662df3f4d
snehalatha06 <111651894+snehalatha06@users.noreply.github.com>
2022-08-20 23:25:24 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,36 @@
+var a=prompt("enter a subject");
+var b=prompt("enter marks");
+var c=prompt("maths");
+var pd=promt("science");
+var e=prompt("physics");
+var f=prompt("chemistry");
+var g=prompt("telugu");
+if(c>90){
+  console.log("grade A");
+}
+else if (d>80){
+  console.log("grade B");
+}
+else if(e>60){
+  console.log("grade C");
+}
+else if (f>50){
+  console.log("grade D");
+}
+else if(g>40){
+  console.log("grade E");
+}
+else{
+  console.log("grade F");
+}
+var a=5{
+greet()
+console.log(var)e
+}
+greet();
+console.log(var)
+
+
+
+
+ 
